4441b7b03057fd8f213979cbb1130456feb12091
DamirGDR <rimad91@yandex.ru>
2026-02-25 14:24:12 +0300

    Преобразование типов1

--- entrypoint.py
+++ entrypoint.py
@@ -150,16 +150,7 @@ def main():
     # Выгрузка t_area_revenue_stats2 pandas. Начало
 
 
-    truncate_t_area_revenue_stats2 = '''
-    DELETE FROM damir.t_area_revenue_stats2
-    WHERE damir.t_area_revenue_stats2."timestamp_hour" >= date_trunc('hour', NOW() + INTERVAL '2 hours') - INTERVAL '2 hours';
-    '''
-    with engine_postgresql.connect() as connection:
-        with connection.begin() as transaction:
-            connection.execute(sa.text(truncate_t_area_revenue_stats2))
-            transaction.commit()
-            print(f"Таблица t_area_revenue_stats2 успешно очищена!")
-    
+
     select_kvt = '''
         SELECT 
             res_tab."timestamp" ,
@@ -430,8 +421,18 @@ def main():
 
     df_orders_kvt_area_res['add_time'] = pd.Timestamp.now()
 
-    x = df_orders_kvt_area_res.groupby('timestamp_hour').agg({'kvt': 'sum'})
-    print('kvt3' + str(x))
+    # x = df_orders_kvt_area_res.groupby('timestamp_hour').agg({'kvt': 'sum'})
+    # print('kvt3' + str(x))
+
+    truncate_t_area_revenue_stats2 = '''
+        DELETE FROM damir.t_area_revenue_stats2
+        WHERE damir.t_area_revenue_stats2."timestamp_hour" >= date_trunc('hour', NOW() + INTERVAL '2 hours') - INTERVAL '2 hours';
+        '''
+    with engine_postgresql.connect() as connection:
+        with connection.begin() as transaction:
+            connection.execute(sa.text(truncate_t_area_revenue_stats2))
+            transaction.commit()
+            print(f"Таблица t_area_revenue_stats2 успешно очищена!")
 
     df_orders_kvt_area_res.to_sql("t_area_revenue_stats2", engine_postgresql, if_exists="append", index=False)
     print('Таблица t_area_revenue_stats2 успешно обновлена!')
@@ -439,15 +440,6 @@ def main():
     # Выгрузка t_area_revenue_stats2 pandas. Конец
 
     # Выгрузка t_area_revenue_stats3 pandas. Начало
-    truncate_t_area_revenue_stats3 = '''
-        DELETE FROM damir.t_area_revenue_stats3
-        WHERE damir.t_area_revenue_stats3."timestamp_hour" >= date_trunc('day', (NOW() + INTERVAL '2 hours')) - INTERVAL '1 days';
-        '''
-    with engine_postgresql.connect() as connection:
-        with connection.begin() as transaction:
-            connection.execute(sa.text(truncate_t_area_revenue_stats3))
-            transaction.commit()
-            print(f"Таблица t_area_revenue_stats3 успешно очищена!")
 
     select_kvt = '''
         SELECT 
@@ -744,6 +736,17 @@ def main():
     df_orders_kvt_area_res['add_time'] = pd.Timestamp.now()
 
     df_orders_kvt_area_res.rename(columns={'start_day': 'timestamp_hour'}, inplace=True)
+
+    truncate_t_area_revenue_stats3 = '''
+        DELETE FROM damir.t_area_revenue_stats3
+        WHERE damir.t_area_revenue_stats3."timestamp_hour" >= date_trunc('day', (NOW() + INTERVAL '2 hours')) - INTERVAL '1 days';
+        '''
+    with engine_postgresql.connect() as connection:
+        with connection.begin() as transaction:
+            connection.execute(sa.text(truncate_t_area_revenue_stats3))
+            transaction.commit()
+            print(f"Таблица t_area_revenue_stats3 успешно очищена!")
+
 
     df_orders_kvt_area_res.to_sql("t_area_revenue_stats3", engine_postgresql, if_exists="append", index=False)
     print('Таблица t_area_revenue_stats3 успешно обновлена!')
